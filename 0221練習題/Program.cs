776bed5229fda70f1df3dec2915ef3a487d2af9e
Dan <dan900306@gmail.com>
2023-02-21 11:55:17 +0800

    加入專案檔案。

---
+++ 0221練習題/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0221練習題
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一串以逗號分隔的字串，並將其反向顯示");
+            Console.Write("輸入: ");
+            string input_str=Console.ReadLine();
+            var words = input_str.Split(',').ToList();
+            //var list = new List<string>();
+            var count= words.Count();
+            for(int i =0; i<count; i++)
+            {
+                words. = i;
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ 第二題/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第二題
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("輸入:");
+            var input = Console.ReadLine();
+           
+            var odd_num = input.Split(',').Where(x => int.Parse(x) % 2 != 0).OrderBy(x=> int.Parse(x));
+            List<int> list2 = new List<int>();
+            var even_num = input.Split(',').Where(x => int.Parse(x) % 2 == 0).OrderBy(x => int.Parse(x));
+            var n =1;
+            foreach (var x in odd_num)
+            {
+                Console.Write(x);
+                if(n<odd_num.Count())
+                {
+                    Console.Write(",");
+                }
+                n++;
+                
+            }
+            Console.WriteLine();
+            n = 1;
+            foreach (var x in even_num)
+            {
+                Console.Write(x);
+                if(n<even_num.Count())
+                {
+                    Console.Write(",");
+                }
+                n++;
+            }
+        }
+    }
+}
